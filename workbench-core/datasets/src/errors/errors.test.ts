4f06996c1160d0658a333e4708f759a0f72b220e
Robert Smayda <smayda44@gmail.com>
2022-07-28 16:20:53 -0400

    feat: initial commit

---
+++ workbench-core/datasets/src/errors/errors.test.ts
@@ -0,0 +1,47 @@
+/*
+ *  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  EndPointExistsError,
+  EndPointTerminatedError,
+  isEndPointExistsError,
+  isEndPointTerminatedError,
+  isRoleExistsOnEndpointError,
+  RoleExistsOnEndpointError
+} from '../';
+
+const error = new Error();
+
+describe('custom error tests', () => {
+  test('endPointExistsError', () => {
+    const endPointExistsError = new EndPointExistsError();
+
+    expect(isEndPointExistsError(endPointExistsError)).toBe(true);
+  });
+
+  test('not endPointExistsError', () => {
+    expect(isEndPointExistsError(error)).toBe(false);
+  });
+
+  test('endPointTerminatedError', () => {
+    const endPointTerminatedError = new EndPointTerminatedError();
+
+    expect(isEndPointTerminatedError(endPointTerminatedError)).toBe(true);
+  });
+
+  test('not endPointTerminatedError', () => {
+    expect(isEndPointTerminatedError(error)).toBe(false);
+  });
+
+  test('RoleExistsOnEndPointError', () => {
+    const roleExistsOnEndPointError = new RoleExistsOnEndpointError();
+
+    expect(isRoleExistsOnEndpointError(roleExistsOnEndPointError)).toBe(true);
+  });
+
+  test('not RoleExistsOnEndPointError', () => {
+    expect(isRoleExistsOnEndpointError(error)).toBe(false);
+  });
+});
