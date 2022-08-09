fa35d38fe8584a29981aaac75572fbcc4e518a45
Tim Nguyen <thingut@amazon.com>
2022-08-09 11:21:10 -0400

    chore: remove merge conflict header

--- solutions/swb-reference/src/SWBStack.ts
+++ solutions/swb-reference/src/SWBStack.ts
@@ -277,10 +277,7 @@ export class SWBStack extends Stack {
         })
       ]
     });
-<<<<<<< HEAD
 
-=======
->>>>>>> develop
     const sagemakerNotebookPolicy = new PolicyDocument({
       statements: [
         new PolicyStatement({
