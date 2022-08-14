532ca45e89807c462c1453aba8922bd9512cffc2
Fernando Aranda <farandac@amazon.com>
2022-08-13 22:16:24 -0700

    feat(environments integration tests): add environments multistep integration tests

--- solutions/swb-reference/integration-tests/support/resources/environments/environment.ts
+++ solutions/swb-reference/integration-tests/support/resources/environments/environment.ts
@@ -5,6 +5,7 @@
 import { EnvironmentStatus } from '@aws/workbench-core-environments';
 import { AxiosResponse } from 'axios';
 import ClientSession from '../../clientSession';
+import { ENVIRONMENT_START_MAX_WAITING_SECONDS } from '../../utils/constants';
 import { sleep } from '../../utils/utilities';
 import Resource from '../base/resource';
 
@@ -13,8 +14,38 @@ export default class Environment extends Resource {
     super(clientSession, 'environment', id, parentApi);
   }
 
+  //eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public async pollEnvironment(
+    pollingIntervalInSeconds: number,
+    maxWaitTimeInSeconds: number,
+    pollingWhile: (env: any) => boolean
+  ): Promise<void> {
+    const startTimeInMs = Date.now();
+    let totalTimeWaitedInSeconds = 0;
+
+    const { data: resource } = await this.get();
+    let currentEnv = resource;
+    try {
+      console.log(`Polling environment ${this._id}. This will take a few minutes.`);
+      while (pollingWhile(currentEnv) && totalTimeWaitedInSeconds < maxWaitTimeInSeconds) {
+        await sleep(pollingIntervalInSeconds);
+        const { data: resource } = await this.get();
+        currentEnv = resource;
+        totalTimeWaitedInSeconds = (Date.now() - startTimeInMs) / 1000;
+      }
+      if (totalTimeWaitedInSeconds >= maxWaitTimeInSeconds)
+        console.log(`Polling for environment ${this._id} exceeded the time limit.`);
+      else console.log(`Polling for environment ${this._id} finished successfully.`);
+    } catch (e) {
+      console.log(
+        `Polling failed for environment. Last known status for env ${this._id} was "${currentEnv.status}". 
+        Waited ${totalTimeWaitedInSeconds} seconds for environment to reach valid state so it could finish polling; encountered error: ${e}`
+      );
+    }
+  }
+
   public async connect(): Promise<AxiosResponse> {
-    return this._axiosInstance.put(`${this._api}/connections`);
+    return this._axiosInstance.get(`${this._api}/connections`);
   }
 
   public async stop(): Promise<AxiosResponse> {
@@ -31,10 +62,6 @@ export default class Environment extends Resource {
 
   protected async cleanup(): Promise<void> {
     const defAdminSession = await this._setup.getDefaultAdminSession();
-    const maxWaitTimeInSeconds = 600;
-    const startTimeInMs = Date.now();
-    let totalTimeWaitedInSeconds = 0;
-
     const { data: resource } = await defAdminSession.resources.environments.environment(this._id).get();
     let envStatus: EnvironmentStatus = resource.status;
     if (['TERMINATED', 'TERMINATING'].includes(envStatus)) {
@@ -43,18 +70,22 @@ export default class Environment extends Resource {
     }
     try {
       console.log(`Attempting to delete environment ${this._id}. This will take a few minutes.`);
-      while (envStatus === 'PENDING' && totalTimeWaitedInSeconds < maxWaitTimeInSeconds) {
-        await sleep(15000);
-        const { data: resource } = await defAdminSession.resources.environments.environment(this._id).get();
-        envStatus = resource.status;
-        totalTimeWaitedInSeconds = (Date.now() - startTimeInMs) / 1000;
-      }
+      await this.pollEnvironment(
+        1500,
+        ENVIRONMENT_START_MAX_WAITING_SECONDS,
+        (env) => env.status === 'PENDING'
+      ); //wating for environment to be completed
+      const { data: completedResource } = await defAdminSession.resources.environments
+        .environment(this._id)
+        .get();
+      envStatus = completedResource.status;
+      console.log(`Terminating environment ${this._id}.`);
       await defAdminSession.resources.environments.environment(this._id).terminate();
       console.log(`Deleted environment ${this._id}`);
     } catch (e) {
       console.log(
         `Could not delete environment. Last known status for env ${this._id} was "${envStatus}". 
-        Waited ${totalTimeWaitedInSeconds} seconds for environment to reach valid state so it could be deleted; encountered error: ${e}`
+        Encountered error: ${e}`
       );
     }
   }
--- solutions/swb-reference/integration-tests/tests/multiStep/environment.test.ts
+++ solutions/swb-reference/integration-tests/tests/multiStep/environment.test.ts
@@ -4,6 +4,11 @@
  */
 import ClientSession from '../../support/clientSession';
 import Setup from '../../support/setup';
+import {
+  ENVIRONMENT_START_MAX_WAITING_SECONDS,
+  ENVIRONMENT_STOP_AND_TERMINATE_MAX_WAITING_SECONDS,
+  ENVIRONMENT_STOP_MAX_WAITING_SECONDS
+} from '../../support/utils/constants';
 import { uuidRegExp } from '../../support/utils/regExpressions';
 
 describe('multiStep environment test', () => {
@@ -19,6 +24,8 @@ describe('multiStep environment test', () => {
   });
 
   test('launch, connect, stop, get, terminate', async () => {
+    //Create Environment A
+    console.log('Creating Environment A');
     const { data: environmentA } = await adminSession.resources.environments.create();
     expect(environmentA).toMatchObject({
       id: expect.stringMatching(uuidRegExp),
@@ -29,6 +36,8 @@ describe('multiStep environment test', () => {
       PROJ: expect.anything() // PROJ should be defined
     });
 
+    //Create Environment B
+    console.log('Creating Environment B');
     const { data: environmentB } = await adminSession.resources.environments.create();
     expect(environmentB).toMatchObject({
       id: expect.stringMatching(uuidRegExp),
@@ -39,15 +48,192 @@ describe('multiStep environment test', () => {
       PROJ: expect.anything() // PROJ should be defined
     });
 
-    // await adminSession.resources.environments.environment(environmentA.id).connect();
-    // await adminSession.resources.environments.environment(environmentA.id).stop();
-    // await adminSession.resources.environments.get({name: environmentA.name, ascending: "name"});
-    // await adminSession.resources.environments.get({status: environmentA.status, ascending: "status"});
-    // await adminSession.resources.environments.get({createdAtFrom: environmentA.createdAt, createdAtTo: environmentA.createdAt, ascending: "createdAt"});
-    // await adminSession.resources.environments.get({owner: environmentA.owner, ascending: "owner"});
-    // await adminSession.resources.environments.environment(environmentA.id).start();
-    // await adminSession.resources.environments.environment(environmentA.id).terminate();
-    // await adminSession.resources.environments.environment(environmentB.id).terminate();
-    // await adminSession.resources.environments.get({});
+    //Verify Completed Environment A
+    console.log('Verify Completed Environment A');
+    await adminSession.resources.environments
+      .environment(environmentA.id)
+      .pollEnvironment(15, ENVIRONMENT_START_MAX_WAITING_SECONDS, (env) => env.status === 'PENDING'); //wait for environmentA to complete
+    const { data: environmentACompleted } = await adminSession.resources.environments
+      .environment(environmentA.id)
+      .get();
+    expect(environmentACompleted).toMatchObject({
+      id: expect.stringMatching(uuidRegExp),
+      status: 'COMPLETED',
+      ETC: expect.anything(), //ETC should be defined
+      PROJ: expect.anything() // PROJ should be defined
+    });
+    console.log('Environment A Completed');
+
+    //Verify Completed Environment B
+    console.log('Verify Completed Environment B');
+    await adminSession.resources.environments
+      .environment(environmentB.id)
+      .pollEnvironment(15, ENVIRONMENT_START_MAX_WAITING_SECONDS, (env) => env.status === 'PENDING'); //wait for environmentB to complete
+    const { data: environmentBCompleted } = await adminSession.resources.environments
+      .environment(environmentB.id)
+      .get();
+    expect(environmentBCompleted).toMatchObject({
+      id: expect.stringMatching(uuidRegExp),
+      status: 'COMPLETED',
+      ETC: expect.anything(), //ETC should be defined
+      PROJ: expect.anything() // PROJ should be defined
+    });
+    console.log('Environment B Completed');
+
+    //Verify Connect Environment A
+    console.log('Verify Connect Environment A');
+    const { data: environmentAConnectInfo } = await adminSession.resources.environments
+      .environment(environmentA.id)
+      .connect();
+    expect(environmentAConnectInfo).toMatchObject({
+      authCredResponse: {
+        url: expect.anything()
+      },
+      instructionResponse: expect.anything()
+    });
+
+    //Stop Environment A
+    console.log('Stopping Environment A');
+    await adminSession.resources.environments.environment(environmentA.id).stop();
+    await adminSession.resources.environments
+      .environment(environmentA.id)
+      .pollEnvironment(15, ENVIRONMENT_STOP_MAX_WAITING_SECONDS, (env) => env.status === 'STOPPING'); //wait for environmentA to stop
+    const { data: environmentAStopped } = await adminSession.resources.environments
+      .environment(environmentA.id)
+      .get();
+    expect(environmentAStopped).toMatchObject({
+      id: expect.stringMatching(uuidRegExp),
+      status: 'STOPPED',
+      ETC: expect.anything(), //ETC should be defined
+      PROJ: expect.anything() // PROJ should be defined
+    });
+    console.log('Environment A Stopped');
+
+    //Search Environment A filtering by name
+    console.log('Searching Environment A filtering by name');
+    const { data: environmentsNameFilter } = await adminSession.resources.environments.get({
+      name: environmentAStopped.name
+    });
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect(
+      environmentsNameFilter?.data?.filter((env: any) => env.id === environmentAStopped.id)?.length
+    ).toBeTruthy();
+
+    //Search Environment A filtering by status
+    console.log('Searching Environment A filtering by status');
+    const { data: environmentsStatusFilter } = await adminSession.resources.environments.get({
+      status: environmentAStopped.status
+    });
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect(
+      environmentsStatusFilter?.data?.filter((env: any) => env.id === environmentAStopped.id)?.length
+    ).toBeTruthy();
+
+    //Search Environment A filtering by created at
+    console.log('Searching Environment A filtering by created at');
+    const { data: environmentsCreatedAtFilter } = await adminSession.resources.environments.get({
+      createdAtFrom: environmentAStopped.createdAt,
+      createdAtTo: environmentAStopped.createdAt
+    });
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect(
+      environmentsCreatedAtFilter?.data?.filter((env: any) => env.id === environmentAStopped.id)?.length
+    ).toBeTruthy();
+
+    //Search Environment A filtering by owner
+    console.log('Searching Environment A filtering by owner');
+    const { data: environmentsOwnerFilter } = await adminSession.resources.environments.get({
+      owner: environmentAStopped.owner
+    });
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect(
+      environmentsOwnerFilter?.data?.filter((env: any) => env.id === environmentAStopped.id)?.length
+    ).toBeTruthy();
+
+    //Start Environment A after being stopped
+    console.log('Starting Environment A after being stopped');
+    await adminSession.resources.environments.environment(environmentA.id).start();
+    await adminSession.resources.environments
+      .environment(environmentA.id)
+      .pollEnvironment(
+        15,
+        ENVIRONMENT_START_MAX_WAITING_SECONDS,
+        (env) => env.status === 'PENDING' || env.status === 'STARTING'
+      ); //wait for environmentA to complete
+    const { data: environmentAStarted } = await adminSession.resources.environments
+      .environment(environmentA.id)
+      .get();
+    expect(environmentAStarted).toMatchObject({
+      id: expect.stringMatching(uuidRegExp),
+      status: 'COMPLETED',
+      ETC: expect.anything(), //ETC should be defined
+      PROJ: expect.anything() // PROJ should be defined
+    });
+    console.log('Environment A Completed');
+
+    //Stop Environments A and B
+    console.log('Stopping Environments A and B');
+    await adminSession.resources.environments.environment(environmentA.id).stop();
+    await adminSession.resources.environments.environment(environmentB.id).stop();
+
+    //Wait for stopeed status for Environment A
+    await adminSession.resources.environments
+      .environment(environmentA.id)
+      .pollEnvironment(15, ENVIRONMENT_STOP_MAX_WAITING_SECONDS, (env) => env.status === 'STOPPING'); //wait for environmentA to stop
+    const { data: environmentAStopped2 } = await adminSession.resources.environments
+      .environment(environmentA.id)
+      .get();
+    expect(environmentAStopped2).toMatchObject({
+      id: expect.stringMatching(uuidRegExp),
+      status: 'STOPPED',
+      ETC: expect.anything(), //ETC should be defined
+      PROJ: expect.anything() // PROJ should be defined
+    });
+    console.log('Environment A Stopped');
+
+    //Wait for stopeed status for Environment B
+    await adminSession.resources.environments
+      .environment(environmentB.id)
+      .pollEnvironment(15, ENVIRONMENT_STOP_MAX_WAITING_SECONDS, (env) => env.status === 'STOPPING'); //wait for environmentB to stop
+    const { data: environmentBStopped } = await adminSession.resources.environments
+      .environment(environmentB.id)
+      .get();
+    expect(environmentBStopped).toMatchObject({
+      id: expect.stringMatching(uuidRegExp),
+      status: 'STOPPED',
+      ETC: expect.anything(), //ETC should be defined
+      PROJ: expect.anything() // PROJ should be defined
+    });
+    console.log('Environment B Stopped');
+
+    //Terminate Environments A and B
+    console.log('Terminating Environments A and B');
+    await adminSession.resources.environments.environment(environmentA.id).terminate();
+    await adminSession.resources.environments.environment(environmentB.id).terminate();
+
+    //Wait for Environments A and B to terminate
+    await adminSession.resources.environments
+      .environment(environmentA.id)
+      .pollEnvironment(
+        15,
+        ENVIRONMENT_STOP_AND_TERMINATE_MAX_WAITING_SECONDS,
+        (env) => env?.status === 'TERMINATING'
+      ); //wait for environmentA to stop
+    await adminSession.resources.environments
+      .environment(environmentB.id)
+      .pollEnvironment(
+        15,
+        ENVIRONMENT_STOP_AND_TERMINATE_MAX_WAITING_SECONDS,
+        (env) => env?.status === 'TERMINATING'
+      ); //wait for environmentB to stop
+
+    //Validate Environments A and B are not retrieved on get all environments call
+    console.log('Searching Environment A filtering by owner');
+    const { data: allEnvironments } = await adminSession.resources.environments.get({});
+    //eslint-disable-next-line @typescript-eslint/no-explicit-any
+    expect(
+      allEnvironments?.data?.filter((env: any) => env.id === environmentA.id || env.id === environmentB.id)
+        ?.length
+    ).toBeFalsy();
   });
 });
